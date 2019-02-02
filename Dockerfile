543626843372e16a43eddf4be4b732d5afb076c2
Arkadiusz Sadowski <arksad95@gmail.com>
2019-02-02 17:54:40 +0100

    docker file added

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM openjdk:8
-ADD
+ADD build/libs/um-spring-boot-docker-0.1.0.jar um-spring-boot-docker-0.1.0.jar
+EXPOSE 7050
+ENTRYPOINT ["java", "-jar", "um-spring-boot-docker-0.1.0.jar"]
--- src/test/kotlin/pg/gda/universemonuments/UniverseMonumentsApplicationTests.kt
+++ src/test/kotlin/pg/gda/universemonuments/UniverseMonumentsApplicationTests.kt
@@ -7,15 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
-import pg.gda.universemonuments.config.security.JWTProvider
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @ActiveProfiles("default")
 class UniverseMonumentsApplicationTests {
 
-	@Autowired
-	lateinit var jwtProvider: JWTProvider
 
 	@Ignore
 	@Test
